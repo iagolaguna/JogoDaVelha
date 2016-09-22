97a53cf9b5ce1eb193ddbb75646ece9c06ad5c14
Iago Laguna <iagolaguna.83@gmail.com>
2016-09-22 19:52:26 -0300

    implementing a Game to contain rules of business

--- src/br/com/mvil/Main.java
+++ src/br/com/mvil/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Montando Tabuleiro...");
         System.out.println("Jogador 1  seu simbolo no tabuleiro é  'X'");
         System.out.println("Jogador 2  seu simbolo no tabuleiro é  'O'");
+
     }
 }
