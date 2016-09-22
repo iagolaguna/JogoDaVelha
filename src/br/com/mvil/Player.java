e92d992815686dded83e604fc3b8d3156f84e8f3
Iago Laguna <iagolaguna.83@gmail.com>
2016-09-22 19:26:49 -0300

    create class player with brand to game

---
+++ src/br/com/mvil/Player.java
@@ -0,0 +1,20 @@
+package br.com.mvil;
+
+/**
+ * Created by Aluno on 22/09/2016.
+ */
+public class Player {
+    private char brand;
+
+    public Player(char brand) {
+        this.brand = brand;
+    }
+
+    public char getBrand() {
+        return brand;
+    }
+
+    public void setBrand(char brand) {
+        this.brand = brand;
+    }
+}
