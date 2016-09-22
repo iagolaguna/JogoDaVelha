de4191ad1569bea02302c7e692ad76736f8ce658
Iago Laguna <iagolaguna.83@gmail.com>
2016-09-22 19:45:00 -0300

    building a init game

--- src/br/com/mvil/Main.java
+++ src/br/com/mvil/Main.java
@@ -3,6 +3,8 @@ package br.com.mvil;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Montando Tabuleiro...");
+        System.out.println("Jogador 1  seu simbolo no tabuleiro é  'X'");
+        System.out.println("Jogador 2  seu simbolo no tabuleiro é  'O'");
     }
 }
