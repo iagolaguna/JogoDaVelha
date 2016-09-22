38b361b348b4ed4966fa997ea5bed0dd6672d56c
Iago Laguna <iagolaguna.83@gmail.com>
2016-09-22 20:29:06 -0300

    save

--- src/br/com/mvil/Game.java
+++ src/br/com/mvil/Game.java
@@ -4,15 +4,25 @@ package br.com.mvil;
  * Created by Aluno on 22/09/2016.
  */
 public class Game {
+    private Player p1;
+    private Player p2;
+
     public Game(){
 
     }
 
     public void init(char brandPlayer1, char brandPlayer2){
-        Player p1 = new Player(brandPlayer1);
-        Player p2 = new Player(brandPlayer2);
+        brandPlayer1 = 'O';
+        brandPlayer2 = 'X';
+        System.out.println("Montando Tabuleiro...");
+        System.out.println("Jogador 1  seu simbolo no tabuleiro é  'X'");
+        System.out.println("Jogador 2  seu simbolo no tabuleiro é  'O'");
+        p1 = new Player(brandPlayer1);
+        p2 = new Player(brandPlayer2);
     }
 
-    
-    public void move(){}
+
+    public void move(){
+
+    }
 }
--- src/br/com/mvil/Main.java
+++ src/br/com/mvil/Main.java
@@ -3,9 +3,6 @@ package br.com.mvil;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Montando Tabuleiro...");
-        System.out.println("Jogador 1  seu simbolo no tabuleiro é  'X'");
-        System.out.println("Jogador 2  seu simbolo no tabuleiro é  'O'");
 
     }
 }
