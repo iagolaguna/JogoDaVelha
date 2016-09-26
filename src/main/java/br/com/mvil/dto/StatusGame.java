de7b8a16eb08778dad629a0bf627342b370afcb6
iagolaguna <iagolaguna.83@gmail.com>
2016-09-26 17:38:47 -0300

    - Change numbers of turn for parse to correct player

--- src/main/java/br/com/mvil/dto/StatusGame.java
+++ src/main/java/br/com/mvil/dto/StatusGame.java
@@ -4,11 +4,11 @@ package br.com.mvil.dto;
  * Created by Aluno on 23/09/2016.
  */
 public enum StatusGame {
-    TURN_PLAYER_ONE(0),
-    TURN_PLAYER_TWO(1),
-    WINNER_PLAYER_ONE(2),
-    WINNER_PLAYER_TWO(3),
-    TIED_PLAYERS(4);
+    TURN_PLAYER_ONE(1),
+    TURN_PLAYER_TWO(2),
+    WINNER_PLAYER_ONE(3),
+    WINNER_PLAYER_TWO(4),
+    TIED_PLAYERS(5);
 
     int value;
     StatusGame(int value) {
