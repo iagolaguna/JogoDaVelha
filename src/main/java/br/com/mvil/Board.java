b86f54897c24b5281bba3749d3a81b5c7d0b5cc7
iagolaguna <iagolaguna.83@gmail.com>
2016-09-26 12:03:02 -0300

    - refactor validations in board, created many validations for specific cases

--- src/main/java/br/com/mvil/Board.java
+++ src/main/java/br/com/mvil/Board.java
@@ -6,7 +6,8 @@ package br.com.mvil;
 
 public class Board {
 
-	private char[][] consoleBoard = new char[3][3];
+    public static final char EMPTY_CHAR = ' ';
+    private char[][] consoleBoard = new char[3][3];
 
 
 	/*
@@ -47,7 +48,7 @@ public class Board {
 	 * @Author Marcos Vinicius
 	 * */
 	private boolean validateCombination(char a, char b, char c){
-		return a==b && b==c &&  a !=' ' ;
+		return a==b && b==c &&  a != EMPTY_CHAR;
 		
 	}
 	
@@ -55,32 +56,38 @@ public class Board {
 	 * @Author Marcos Vinicius
 	 * */
 	public boolean validateGame(){
+        return  validateHorizontal() && validateVertical() && validateDiagonal() && validateDiagonalReverse();
+    }
 
-		for(int x=0;x<3;x++){
-			if(validateCombination(consoleBoard[x][0],consoleBoard[x][1],consoleBoard[x][2])){
-				return true;
-			} 
-		}
-		
-		for(int y=0;y<3;y++){
+    private boolean validateDiagonalReverse() {
+        return  validateCombination(consoleBoard[0][2],consoleBoard[1][1],consoleBoard[2][0]);
+    }
+
+    private boolean validateDiagonal() {
+        return validateCombination(consoleBoard[0][0],consoleBoard[1][1],consoleBoard[2][2]);
+    }
+
+    private boolean validateHorizontal() {
+        for(int y=0;y<3;y++){
 			if(validateCombination(consoleBoard[0][y],consoleBoard[1][y],consoleBoard[2][y])){
-				return true;
+                return true;
 			}
 		}
-		
-		if(validateCombination(consoleBoard[0][0],consoleBoard[1][1],consoleBoard[2][2])){
-			return true;
-		}
-		
-		if(validateCombination(consoleBoard[0][2],consoleBoard[1][1],consoleBoard[2][0])){
-			return true;
-		}
-		
-		return false;
-	}
-	/*
-	 * @Author Marcos Vinicius
-	 * */
+        return false;
+    }
+
+    private boolean validateVertical() {
+        for(int x=0;x<3;x++){
+            if(validateCombination(consoleBoard[x][0],consoleBoard[x][1],consoleBoard[x][2])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+     * @Author Marcos Vinicius
+     * */
 	public boolean isPositionEmpty(int x, int y){
 		return isCharEmpty(consoleBoard[x][y]);
 	}
--- src/test/java/ValidateCombinationTest.java
+++ src/test/java/ValidateCombinationTest.java
@@ -1,14 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runner.Runner;
-import org.junit.runners.JUnit4;
 
 /**
  * Created by iago.laguna on 26/09/2016.
  */
 
-public class ValidatreCombinationTest {
+public class ValidateCombinationTest {
 
     @Test
     public void validateTest(){
