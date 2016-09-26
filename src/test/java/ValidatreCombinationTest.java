c923fb09967b20c584bc88fee7517964f8b8792a
iagolaguna <iagolaguna.83@gmail.com>
2016-09-26 08:45:07 -0300

    - Add JUnit for implementing tests automated - Refactoring structure of code

---
+++ src/test/java/ValidatreCombinationTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by iago.laguna on 26/09/2016.
+ */
+
+public class ValidatreCombinationTest {
+
+    @Test
+    public void validateTest(){
+        Assert.assertEquals(true,true);
+    }
+}
